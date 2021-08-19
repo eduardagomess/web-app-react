b65b9f4c821bf3e8bb6b5bcfd0c4e48f67a95726
Eduarda <eduardaoliveiragomess@gmail.com>
2021-08-19 09:13:10 -0300

    Update home.js, it was added title and navbar

---
+++ web-app/src/pages/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import '../App.css'
+
+
+function Home(){
+    return (
+    <div id="bg">
+        <Navbar/>
+        <div>
+            <h1 id="title">Descubra a sequência de primos entre dois números de forma ágil!</h1>
+        </div>
+    </div>
+    )
+};
+
+export default Home;
