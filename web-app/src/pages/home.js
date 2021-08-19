ea4f9618e11c180910764dabff88a57791524b77
Eduarda <eduardaoliveiragomess@gmail.com>
2021-08-19 10:03:24 -0300

    Create form

--- web-app/src/pages/home.js
+++ web-app/src/pages/home.js
@@ -14,6 +14,17 @@ function Home(){
         <div className='mobile-hide'>
             <Curiosity/>
         </div>
+        <form id='form'>
+            <div id='first-number'>
+                <label id="label"> Primeiro número <input   className='input' type="text" name="num1" id="num1"  /> </label>
+            </div>
+            <div id='second-number'>
+                <label id="label" > Segundo número <input  className='input' name="num2" id="num2" /> </label>
+            </div>
+            <div>
+                <input type="submit" value="Salvar" />
+            </div>
+        </form>
     </div>
     )
 };
