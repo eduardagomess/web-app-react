66f73af930b2df5dc691c28ce58f5fdfaf75476d
Eduarda <eduardaoliveiragomess@gmail.com>
2021-08-19 18:08:34 -0300

    Update App.css and App.js, changed button

--- web-app/src/pages/home.js
+++ web-app/src/pages/home.js
@@ -29,8 +29,8 @@ function Home(){
         <Navbar/>
         <div id="result">
             <ul>
-                <li className='list'>
-                    <table id='table'>
+                <li className="list">
+                    <table id="table">
                         <tr><td>
                             <spam className="result-list">Resultado {result}</spam>
                         </td></tr>
@@ -39,21 +39,22 @@ function Home(){
             </ul>
         </div>
         <div>
-            <h1 id='title'>Descubra a sequência de primos entre dois números de forma ágil!</h1>
+            <h1 id="title">Descubra a sequência de primos entre dois números de forma ágil!</h1>
         </div>
-        <div className='mobile-hide'>
+        <div className="mobile-hide">
             <Curiosity/>
         </div>
-        <form id='form' onSubmit={handleFormSubmit}>
-            <div id='first-number'>
-                <label id="label"> Primeiro número <input className='input'  name='num1' id='num1' onChange={handleInputChange}  required /> </label>
+        <form id="form" onSubmit={handleFormSubmit}>
+            <div id="first-number">
+                <label id="label"> Primeiro número <input className="input" type="number"  name="num1" id="num1" onChange={handleInputChange}   required /> </label>
             </div>
-            <div id='second-number'>
-                <label id="label" > Segundo número <input  className='input' name='num2' id='num2' onChange={handleInputChange}  required /> </label>
+            <div id="second-number">
+                <label id="label" > Segundo número <input  className="input" type="number" name="num2" id="num2" onChange={handleInputChange}   required /> </label>
             </div>
             <div>
-            { /*<button id='button' type='submit' value='Salvar'><spam id='button-title'>Enviar</spam></button>*/}
-              <input id='st' type="submit" value="Salvar"></input>
+                <button id="button" type="submit" value="Salvar">
+                    <span id="button-title">Enviar</span>
+                </button>
             </div>
         </form>
     </div>
